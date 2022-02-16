fabd02b35c86474ff4657ee54517af4f2b13cacc
eamoses <e.a.moses@gmail.com>
2022-02-15 19:58:20 -0600

    saving calculator

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let operation = null
 
 // this function takes in the number you type in the input field and saves it to the "firstNum" variable
 const saveFirstNumber = (num) => {
-  firstNum = parseInt(num)  
+  firstNum = parseInt(num)
 }
 
 // this function takes in the number you type in the 2nd input field and saves it to the "secondNum" variable
@@ -30,16 +30,28 @@ const subtract = (numA, numB) => {
 // These variables are already defined but that don't point to functions. It's up to you to build the functions to complete your calculator use:
 
 const multiply = (numA, numB) => {
+  const product = numA * numB
+  return product
   // * to get a product then return it
   // Open up the inspector tool in Chrome and select the Console tab to see what this functions is "logging out" to the console.
-  console.log(numA, numB)
 }
 
-const divide = null
-// / to get a quotient,
+const divide = (numA, numB) => {
+  // / to get a quotient
+  if (numB == 0) {
+    const quotient = "You can't divide by zero."
+    return quotient
+  } else {
+    const quotient = numA / numB
+    return quotient
+  }
+}
 
-const modulus = null
-// and % to get a remainder.
+const modulus = (numA, numB) => {
+  // and % to get a remainder.
+  const remainder = numA % numB
+  return remainder
+}
 
 // This function changes the "operation" variable to be equal to the "id" of the button we choose on the web page.
 const changeOperation = (chosenOperation) => {
@@ -51,8 +63,8 @@ const changeOperation = (chosenOperation) => {
 // In order to show the user their results we have to access the DOM and stick in the value
 const putResultInElement = (operationResults) => {
   // access the DOM by writing "document" then use the method "getElementById" and pass it the id, "result".
-  document.getElementById("result").innerHTML = "Results: " + operationResults
-
+  document.getElementById("result").innerHTML = `Results: ${operationResults}`
+  // "Results: " + operationResults
   // Remember, each element has built in properties like "innerHTML" which we can change to anything we like. 
   // Here we give it a string: "Results: " and add the value of the operation to it.
 }
@@ -64,13 +76,12 @@ const equals = () => {
     break;
     case "subtraction": putResultInElement(subtract(firstNum, secondNum)) 
     break;
-    case "multiplication": multiply(firstNum, secondNum) 
+    case "multiplication": putResultInElement(multiply(firstNum, secondNum))
     break;
-    case "division": console.log(divide(firstNum, secondNum)) 
+    case "division": putResultInElement(divide(firstNum, secondNum))
     break;
-    case "modulus": console.log(modulus(firstNum, secondNum)) 
+    case "modulus": putResultInElement(modulus(firstNum, secondNum))
     break;
     default: "Choose an operation"
   }
 }
-
